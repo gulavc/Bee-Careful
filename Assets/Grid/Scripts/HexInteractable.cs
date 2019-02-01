64b324f946e7afb049ace25f9d6daa3e41cba8df
Capi <glavoieh@gmail.com>
2019-02-01 11:50:08 -0500

    Unit trigger system

--- Assets/Grid/Scripts/HexInteractable.cs
+++ Assets/Grid/Scripts/HexInteractable.cs
@@ -2,8 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HexInteractable : MonoBehaviour {
+public abstract class HexInteractable : MonoBehaviour {
 
-    private HexGrid hexgrid;
+    public HexCell Cell { get; set; }
+
+    private bool activateAction, actionDone;
+
+    void Start()
+    {
+        activateAction = false;
+        actionDone = false;
+    }
+
+    void Update()
+    {
+        if (Cell.Unit)
+        {
+            activateAction = true;
+        }
+        else
+        {
+            activateAction = false;
+            actionDone = false;
+        }
+
+        if (activateAction && !actionDone)
+        {
+            OnUnitEnterCell(Cell);
+            actionDone = true;
+        }
+    }
+
+    public abstract void OnUnitEnterCell(HexCell cell);
 
 }
