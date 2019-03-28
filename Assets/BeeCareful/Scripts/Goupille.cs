6eef2b3d941c111a12565edd5873a93d0e864b6a
Guillaume Lavoie <glavoieh@gmail.com>
2019-03-28 11:53:51 -0400

    bugfix goupille

--- Assets/BeeCareful/Scripts/Goupille.cs
+++ Assets/BeeCareful/Scripts/Goupille.cs
@@ -7,8 +7,11 @@ public class Goupille : MonoBehaviour {
     HexMapCamera hexMapCamera;
     Transform attachedObject;
     SpriteRenderer spriteRenderer;
+    HexGrid grid;
+
     public float upperLimit;
     public float lowerLimit;
+    public GoupilleType type;
 
     private float regression, intercept;
 
@@ -17,6 +20,7 @@ public class Goupille : MonoBehaviour {
         hexMapCamera = FindObjectOfType<HexMapCamera>();
         attachedObject = transform.parent;
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        grid = FindObjectOfType<HexGrid>();
 
         regression = 1f / (upperLimit - lowerLimit);
         intercept = 1 - regression * upperLimit;
@@ -35,8 +39,8 @@ public class Goupille : MonoBehaviour {
         float lerp = (regression * hexMapCamera.Zoom) + intercept;
 
         Color c = spriteRenderer.color;
-
-        if (HexGrid.instance.GetCell(attachedObject.position).IsVisible)
+        
+        if (grid.GetCell(attachedObject.localPosition).IsExplored)
         {
             c.a = Mathf.Lerp(0f, 1f, lerp);
         }
@@ -47,5 +51,14 @@ public class Goupille : MonoBehaviour {
 
         spriteRenderer.color = c;        
 
+    }
+
+    public enum GoupilleType
+    {
+        BEE,
+        HIVE,
+        RESOURCE,
+        MOUNTAIN,
+        DANGER
     }
 }
