a09ed022a3c9cf7ef1226b9431c80c96cfa4bfec
Alexandre Poirier <poirier.alex.3@gmail.com>
2019-04-17 13:11:29 -0400

    stuffffffffffffffffff

---
+++ Assets/UpgradeAlert.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeAlert : MonoBehaviour {
+
+    public UpgradeManager upgradeManager;
+
+
+    private List<string> upgradeList;
+    private List<string> upgradeNames;
+
+    private Vector3 startPosition;
+
+    public float showTime = 3f;
+    
+    // Use this for initialization
+    void Start () {
+        upgradeNames = new List<string>();
+        upgradeList = new List<string>();
+        startPosition = this.transform.position;
+
+        //TEST
+        Show();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public bool VerifyPopup()
+    {
+        if (upgradeNames.Count == 0)
+        {
+            upgradeNames = upgradeManager.GetUpgradeNames();
+        }
+
+        List<string> newUpgrades = new List<string>();
+
+        foreach(string upgrade in upgradeNames)
+        {
+            if (VerifyUpgrade(upgrade))
+            {
+                newUpgrades.Add(upgrade);
+            }
+        }
+
+        bool shouldPopup = false;
+
+        foreach(string newUp in newUpgrades)
+        {
+            if (!upgradeList.Contains(newUp))
+            {
+                shouldPopup = true;
+            }
+        }
+
+        upgradeList = newUpgrades;
+
+        return shouldPopup;
+    }
+
+
+    private bool VerifyUpgrade(string s)
+    {
+        if (upgradeManager.IsUpgradeAcquired(s))
+        {
+            return false;
+        }
+
+        if(upgradeManager.IsUpgradeBuyable(s) && upgradeManager.IsUpgradeAvailable(s))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+
+    public void Show()
+    {
+        StartCoroutine(Translate(Vector2.right));
+    }
+
+    public void Hide()
+    {
+
+    }
+
+    IEnumerator Translate(Vector2 direction)
+    {
+        while (true)
+        {
+            this.transform.Translate(Vector3.right);
+
+            yield return null;
+        }
+    }
+
+}
